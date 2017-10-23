b3ae95774bd8284fd88ba3566d556a95bbebe698
madhav175 <madhav897@gmail.com>
2017-10-23 18:20:42 -0400

    Added cfg object method for merge file code to verify merge fil
    e parameters

--- src/filemanagement/FileMerge.java
+++ src/filemanagement/FileMerge.java
@@ -2,14 +2,32 @@ package filemanagement;
 
 import java.io.*;
 
+import init.CommonConfig;
+
 /* FileMerge takes name of the file to while all the split files should be merged
+ * initProperties() - takes output file name and create cfg object
  * mergeFiles() - looks into splitParts folder reads each file and writes into output file
  */
 public class FileMerge {
 	public static File outFile = null;
+	public static String outFileName = null;
+	public static CommonConfig cfg = null;
 
 	public FileMerge(String oFName){
+		outFileName = oFName;
 		outFile = new File(oFName);
+	}
+
+	/* here cfg object can be used to check the size of combined file
+	 * whether it is equal to all split files combined
+	 * whether the size of each piece is equal to given value or not etc
+	 */
+
+	public static void initProperties(){
+		//create a config object with the output file name
+		//initialize the properties
+		cfg = new CommonConfig(outFileName);
+		cfg.init();
 	}
 
 	public static void mergeFiles(){
